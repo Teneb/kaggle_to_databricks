37a502d6261128a41d4dcbb0c8c9471f3f7fcb3f
Teneb <63679429+Teneb@users.noreply.github.com>
2023-05-12 09:11:09 +0200

    Delete KaggleToDatabricks directory

--- KaggleToDatabricks/__innit__.py
+++
@@ -1,5 +0,0 @@
-from KaggleToDatabricks.Authenticate import Authenticate
-from KaggleToDatabricks.TransformDataset import TransformDataset,
-
-from KaggleToDatabricks.test_Authenticate import TestAuthenticate
-from KaggleToDatabricks.test_TransformDataset import TestTransformDataset
